0c9e02d7766f894ef8ba8d008361b366c8e48420
Daniel Nordhoff-Vergien <dve@vergien.net>
2020-06-23 11:31:32 +0200

    let webcomponens exporter servlet extends vaadin servlet

--- src/main/java/net/vergien/daniel/WebComponentVaadinServlet.java
+++ src/main/java/net/vergien/daniel/WebComponentVaadinServlet.java
@@ -2,8 +2,10 @@ package net.vergien.daniel;
 
 import javax.servlet.annotation.WebServlet;
 
+import com.vaadin.flow.server.VaadinServlet;
+
 @WebServlet(urlPatterns = { "/vaadin/*" })
-public class WebComponentVaadinServlet
+public class WebComponentVaadinServlet extends VaadinServlet
 {
 
 }
